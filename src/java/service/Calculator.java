5f8eaf0a2a5fd273fe50dcad3ad40d5d488d4f3b
Agilead <agileadvn@gmail.com>
2017-12-17 10:55:12 +0700

    Add new Calculator and test

---
+++ src/java/service/Calculator.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+/**
+ *
+ * @author hien
+ */
+public class Calculator {
+    public int sum(int number1, int number2) {
+        return number1 + number2;
+    }
+}
---
+++ test/service/CalculatorTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author hien
+ */
+public class CalculatorTest {
+    
+    public CalculatorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of sum method, of class Calculator.
+     */
+    @Test
+    public void testSum() {
+        int number1 = 10;
+        int number2 = 20;
+        Calculator instance = new Calculator();
+        int expResult = 30;
+        int result = instance.sum(number1, number2);
+        assertEquals(expResult, result);
+    }
+    
+}
